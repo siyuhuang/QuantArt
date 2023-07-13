71fecbc94bbf9f4698f30a291a545f3779557887
Siyu Huang <30685235+siyuhuang@users.noreply.github.com>
2023-07-13 08:39:21 -0400

    Add files via upload

--- taming/data/paired_image.py
+++ taming/data/paired_image.py
@@ -27,7 +27,16 @@ class PairedImageTrain(PairedImageBase):
         else:
             paths = None
         self.data = PairedImagePaths(paths=paths, folder1=folder1, folder2=folder2, size=size, random_crop=False)
-
+        
+class PairedImageVal(PairedImageBase):
+    def __init__(self, size, training_images_list_file=None, folder1=None, folder2=None):
+        super().__init__()
+        if training_images_list_file is not None:
+            with open(training_images_list_file, "r") as f:
+                paths = f.read().splitlines()
+        else:
+            paths = None
+        self.data = PairedImagePaths(paths=paths, folder1=folder1, folder2=folder2, size=size, random_crop=False)
 
 class PairedImageTest(PairedImageBase):
     def __init__(self, size, test_images_list_file=None, folder1=None, folder2=None):
